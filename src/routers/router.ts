c061625ba0c2565c9bb97b41f4575e2fe3d1579b
ducanh2710203 <duc2710203@gmail.com>
2023-06-22 15:35:08 +0700

    build profileUser.controller.ts

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -2,7 +2,10 @@ import express from "express";
 import { Router } from "express"
 const router = Router()
 import HomeController from "../controllers/home.controller";
+import ProfileUserController from "../controllers/profileUser.controller";
 
 router.get('/', HomeController.getHomePage)
+router.get('/ProfileUser', ProfileUserController.getManagerUserPage)
+
 
 export default router
