e5106b3a155647482fbbcbdfff281c87ced12e45
Phong <ngphong.0708@gmail.com>
2023-06-22 16:41:48 +0700

    login

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import express = require('express');
 import bodyParser from "body-parser";
 import path from "path";
 import router from "./src/routers/router";
+import passport from "passport";
+import session from "express-session";
+import livereload from "connect-livereload";
 import {ConnectDB} from "./src/models/ConnectDB";
 
 const app = express();
@@ -23,11 +26,29 @@ app.use(bodyParser.urlencoded({ extended : true}))
 app.use(express.static('./src/views'))
 app.use(express.static('another_static_folder'))
 app.use(express.static('./src/public'))
-
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use(livereload());
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine',"ejs")
 app.set("views", "./src/views")
-
 app.use(router);
+app.use('/',router);
+// viet middlewares chinh sua res
+
+// app.use((req: any, res: any, next: any)=> {
+//     if (req.isAuthenticated()) {
+//         res.locals.userLogin = req.user
+//         next();
+//     } else {
+//         res.redirect('/login')
+//     }
+// })
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`)
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,37 @@
 import express from "express";
 import { Router } from "express"
+import passport from "../middlewares/home.middlewares";
 const router = Router()
+
 import HomeController from "../controllers/home.controller";
+import {user} from "../models/schemas/user.model";
 
 router.get('/', HomeController.getHomePage)
+
+
+router.get('/login', HomeController.getLoginPage);
+router.post('/login',(req,res)=>{
+    console.log(123123)
+})
+// router.post('/login', passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureRedirect: '/login'
+// }));
+// router.get(
+//     '/login/google',
+//     passport.authenticate('google', { scope: ['profile', 'email'] })
+// );
+// router.get(
+//     '/google/callback',
+//
+//     passport.authenticate('google'),
+//
+//     (req, res) => {
+//         res.send('You are authenticated');
+//     }
+// );
+
+
+
 
 export default router
