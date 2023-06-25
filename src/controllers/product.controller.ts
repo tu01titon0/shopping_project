c53298a5f40f1619e63e83433adb6771dde62c12
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-25 11:29:50 +0700

    done product detail

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,9 +1,11 @@
 import {Product} from "../models/schemas/product.model";
+import {Image} from "../models/schemas/image.model";
 
 export class ProductController {
     static async productDetail(req, res) {
         const productID = req.query.id;
         const product = await Product.findOne({_id: productID}).populate('category_id');
-        res.render('productDetail', {user: req.user, product});
+        const images = await Image.find({product_id: productID});
+        res.render('productDetail', {user: req.user, product, images});
     }
 }
