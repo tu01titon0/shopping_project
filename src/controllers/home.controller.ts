814114fc369f4abe361c719766335ec626b44199
ducanh2710203 <duc2710203@gmail.com>
2023-06-26 08:55:54 +0700

    do trang home

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,6 +1,16 @@
+import {Product} from "../models/schemas/product.model";
+import {Image} from "../models/schemas/image.model";
+
 class HomeController {
-    static getHomePage(req: any, res: any): any {
-        res.render('index', {user: req.user});
+    static async getHomePage(req: any, res: any) {
+        try{
+            const productID = req.params.id;
+            const product = await Product.findOne({_id: productID}).populate('category_id');
+            const images = await Image.find({product_id: productID});
+            return  res.render('index', {user: req.user, product, images});
+        }catch (err){
+            console.log(err)
+        }
     }
     static getLoginPage(req: any, res: any): any {
         res.render('login', {user: req.user})
