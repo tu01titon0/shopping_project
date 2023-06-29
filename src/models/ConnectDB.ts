d4a33bb9e2414c12d850e16e32e26e821640e632
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-29 16:05:56 +0700

    update

--- src/models/ConnectDB.ts
+++ src/models/ConnectDB.ts
@@ -8,7 +8,7 @@ import {Image} from "./schemas/image.model";
 export class ConnectDB {
     async connect() {
         // await mongoose.connect('mongodb://127.0.0.1:27017/shopping_project');
-        await mongoose.connect('mongodb+srv://trankhiem99999:bncvznczvzz1411@cluster0.7tvwpiv.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://trankhiem99999:bncvznczvzz1411@cluster0.7tvwpiv.mongodb.net/?retryWrites=true&w=majority/test');
 
 //         async function createDummyData() {
 //             try {
