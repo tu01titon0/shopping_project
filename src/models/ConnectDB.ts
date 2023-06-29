2e297ad9f7516bf523a544eb94fc3f3dad4fd89f
tu <nguyenmanhtu09042000@gmail.com>
2023-06-29 11:45:09 +0700

    fix-css-home

--- src/models/ConnectDB.ts
+++ src/models/ConnectDB.ts
@@ -7,8 +7,8 @@ import {Image} from "./schemas/image.model";
 
 export class ConnectDB {
     async connect() {
-        await mongoose.connect('mongodb://127.0.0.1:27017/shopping_project');
-        // await mongoose.connect('mongodb+srv://tu01titon0:Tu123@cluster0.s5zm4tu.mongodb.net/');
+        // await mongoose.connect('mongodb://127.0.0.1:27017/shopping_project');
+        await mongoose.connect('mongodb+srv://trankhiem99999:bncvznczvzz1411@cluster0.7tvwpiv.mongodb.net/?retryWrites=true&w=majority');
 
 //         async function createDummyData() {
 //             try {
